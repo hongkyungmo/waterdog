d59daf773962bdabdb581fe2ad3cac4153b8dbd1
KimYoenyoung <ujoa95@gmail.com>
2016-04-14 09:02:11 +0900

    .blockserviceimpl

--- musicblock/src/main/java/service/block/impl/BlockServiceImpl.java
+++ musicblock/src/main/java/service/block/impl/BlockServiceImpl.java
@@ -2,6 +2,30 @@ package service.block.impl;
 
 import service.block.BlockService;
 
+@Service("blockServiceImpl")
 public class BlockServiceImpl implements BlockService{
+
+	
+	//Field
+		@Autowired
+		@Qualifier("blockDaoImpl")
+		private BlockDao blockDao;
+		public void setBlockDao(BlockDao blockDao) {
+			this.blockDao = blockDao;
+		}
+		
+		///Constructor
+		public BlockServiceImpl() {
+			System.out.println(this.getClass());
+		}
+
+		///Method
+		public void addBlock(Block block) throws Exception {
+			blockDao.addBlock(block);
+		}
+
+		public void updateBlock(Block block) throws Exception {
+			blockDao.updateBlock(block);
+		}
 
 }
