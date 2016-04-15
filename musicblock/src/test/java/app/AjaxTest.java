be6b3e0b3428f65f6ffcbfd14af752223162c4f1
YangSeungGIl <bewitchsk@gmail.com>
2016-04-15 16:17:18 +0900

    .

--- musicblock/src/test/java/app/AjaxTest.java
+++ musicblock/src/test/java/app/AjaxTest.java
@@ -1,13 +1,16 @@
 package app;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import service.block.BlockService;
 import service.block.impl.BlockServiceImpl;
@@ -32,42 +35,45 @@ public class AjaxTest {
 	}
 
 	/////////////////////////////////////////////////////////////
-	@RequestMapping(value = "ajaxTest", method = RequestMethod.POST)
-	
-	public @ResponseBody void addJsonUser(@RequestBody User user, Model model) throws Exception {
+	@RequestMapping(value = "ajaxTest/{value}", method = RequestMethod.POST)
+	public void addJsonUserGET(@PathVariable String value, @RequestBody Map user, Model model) throws Exception {
 
+		System.out.println("value : " + value);
 		System.out.println("User : " + user);
 		// Business Logic
 		// -> xxxService.addYYY(zzz);
-		// user.setNick("testNick");
-
-		// model.addAttribute("JSON", user);
+		
+		model.addAttribute(user);
+		System.out.println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
 
 	}
 
-	@RequestMapping(value = "ajaxTest", method = RequestMethod.GET)
-	public void addJsonUserGET(Model model) throws Exception {
+	@RequestMapping(value = "ajaxTest/{value}", method = RequestMethod.GET)
+	public void addJsonUserPOST(@PathVariable String value, Model model) throws Exception {
 
-		System.out.println("ajaxTest : ");
+		System.out.println("ajaxTest : " + value);
 		// Business Logic
 		// -> xxxService.addYYY(zzz);
 		// User user = new User();
 		// user.setNick("ha11");
 		//
 		// userService.addUser(user);
-		//BlockService blockService = new BlockServiceImpl();
-		Block block = new Block();
-		block.setdCount(1);
-		block.setEmotion(1);
-		block.setNote("도1,레2,레1");
-		block.setpCount(0);
-		block.setdCount(0);
-		block.setuCode(1);
+
 		
-		System.out.println(block.toString());
+//		//BlockService blockService = new BlockServiceImpl();
+//		Block block = new Block();
+//		block.setdCount(1);
+//		block.setEmotion(1);
+//		block.setNote("도1,레2,레1");
+//		block.setpCount(0);
+//		block.setdCount(0);
+//		block.setuCode(1);
+//		
+//		System.out.println(block.toString());
+//		
+//		blockService.addBlock(block);
 		
-		blockService.addBlock(block);
-		
+
 		model.addAttribute("user", "aaa11");
 
 		System.out.println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
