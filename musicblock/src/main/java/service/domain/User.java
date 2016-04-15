a8cce143bee25b9419f478c4b589711081a32e29
YangSeungGIl <bewitchsk@gmail.com>
2016-04-15 16:02:36 +0900

    생성자 추가

--- musicblock/src/main/java/service/domain/User.java
+++ musicblock/src/main/java/service/domain/User.java
@@ -1,8 +1,19 @@
 package service.domain;
 
+
 public class User {
 	private	int uCode;		// User Code
 	private String nick;	// Nickname
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(int uCode, String nick) {
+		super();
+		this.uCode = uCode;
+		this.nick = nick;
+	}
 
 	public int getUCode() {
 		return uCode;
