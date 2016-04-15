9910c13b74f2e8300d01a57474ed79aadc1e6ae3
keheunhye <asopn1994@daum.net>
2016-04-15 11:50:05 +0900

    addBlock -> 주석추가

--- musicblock/src/main/java/service/block/impl/BlockDaoImpl.java
+++ musicblock/src/main/java/service/block/impl/BlockDaoImpl.java
@@ -22,6 +22,9 @@ public class BlockDaoImpl implements BlockDao{
 	public void addBlock(Block block) {
 		sqlSession.insert("BlockMapper.addBlock", block);
 		sqlSession.insert("BlockMapper.addBlockHash", block.getblockHashList());
+		//Dao에서 for문을 통해 매번 addBlockHash를 부르지 않는 이유 :
+		//dao 함수 호출 - mapper id 검색 - db 오픈 - query 수행 >> 이 매번 일어나니까
+		//지금 방식으로 하면 query 수행 전의 과정이 한번만 발생하고 query 수행만 여러번!
 	}
 
 	@Override
