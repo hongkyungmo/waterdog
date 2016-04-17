0d38c3e8c7b628a5341b68cef8094fe670c7860c
hongkyungmo <withious@naver.com>
2016-04-17 19:14:47 +0900

    공통모듈 : 메뉴바 js파일로 따로 뺌

---
+++ musicblock/src/main/webapp/javascript/common/commonMenu.js
@@ -0,0 +1,6 @@
+/**
+ * 공통모듈 : 페이지 로딩시 자동으로 불러내는 메뉴바 추가
+ */
+
+$("body").append("<div id='menu-container'><span class = 'glyphicon glyphicon-chevron-left fa-2x' aria-hidden = 'true' id='back'></span><span class='glyphicon glyphicon-menu-hamburger' aria-hidden='true' id='menu'></span></div>");
+
