4c2115196d245d1ffd65376b400a484b4ff23835
hongkyungmo <withious@naver.com>
2016-04-17 19:37:06 +0900

    공통모듈 적용(blockMaking.html, template.html)

--- musicblock/src/main/webapp/javascript/common/commonMenu.js
+++ musicblock/src/main/webapp/javascript/common/commonMenu.js
@@ -3,4 +3,3 @@
  */
 
 $("body").append("<div id='menu-container'><span class = 'glyphicon glyphicon-chevron-left fa-2x' aria-hidden = 'true' id='back'></span><span class='glyphicon glyphicon-menu-hamburger' aria-hidden='true' id='menu'></span></div>");
-
