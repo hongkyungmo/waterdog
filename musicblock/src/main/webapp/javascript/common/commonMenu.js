fd52c053ba88600f9da0b10d5c63946138837205
hongkyungmo <withious@naver.com>
2016-04-21 01:22:30 +0900

    메뉴바 콜백함수 추가

--- musicblock/src/main/webapp/javascript/common/commonMenu.js
+++ musicblock/src/main/webapp/javascript/common/commonMenu.js
@@ -3,3 +3,17 @@
  */
 
 $("body").append("<div id='menu-container'><span class = 'glyphicon glyphicon-chevron-left fa-2x' aria-hidden = 'true' id='back'></span><span class='glyphicon glyphicon-menu-hamburger' aria-hidden='true' id='menu'></span></div>");
+
+//menu
+$(function () {
+    $("#menu").bind("mousedown", function () {
+        if ($(".menu-on").length > 0) {
+            $(".menu-on").addClass("menu-off");
+            $(".menu-on").removeClass("menu-on");
+        } else {
+            $(".menu-off").addClass("menu-on");
+            $(".menu-off").removeClass("menu-off");
+            /*$("body").append('<div style="height:92%;width:80%;background-color:gray;position:absolute;left:0%;top:8%;z-index:1000;opacity:0.7">zzzz</div>');*/
+        }
+    });
+});
