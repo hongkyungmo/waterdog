bb9a754ab7624cdeb6c4371e4aef4055c719dc29
hongkyungmo <withious@naver.com>
2016-04-20 21:18:30 +0900

    undefined가 playNote로 들어가는 오류 해결

--- musicblock/src/main/webapp/javascript/blockMaking/blockPlay.js
+++ musicblock/src/main/webapp/javascript/blockMaking/blockPlay.js
@@ -1,18 +1,3 @@
-//음 코드(1~48)를 넣으면 1초간 음을 연주해주는 함수
-var playBlockNote = function (noteVal) {
-    oscillator.frequency.value = noteVal;
-    
-    gain.gain.value = 2;
-    setTimeout(function () {
-        /*oscillator.connect(gain);
-        oscillator.disconnect(gain);*/
-        //gain.gain.value=0;
-    }, 1000);
-    //테스트코드
-    console.log(noteVal);
-}
-
-
 // play버튼 눌러서 블럭 연주
 $(function () {
     // 블럭연주 - 음
@@ -21,14 +6,16 @@ $(function () {
         if (clickSequence != 0) {
             countForPlaying = clickSequence;
             console.log(noteCodeToFreq(noteArr[clickSequence-countForPlaying]));
-            playBlockNote(noteCodeToFreq(noteArr[clickSequence-countForPlaying]));
+            playNote(noteCodeToFreq(noteArr[clickSequence-countForPlaying]));
             timerIdForPlaying = setInterval(function(){
                 //playNote(noteCodeToFreq(13));
                 countForPlaying--;
                 console.log(countForPlaying);
-                playBlockNote(noteCodeToFreq(noteArr[clickSequence-countForPlaying]));
+                if(noteArr[clickSequence-countForPlaying] != undefined){
+                    playNote(noteCodeToFreq(noteArr[clickSequence-countForPlaying]));
+                }
                 if(countForPlaying == 0){
-                    alert("end!!");
+                    //alert("end!!");
                     clearInterval(timerIdForPlaying);
                     mainVolume = 0;
                     gain.gain.value = mainVolume;
