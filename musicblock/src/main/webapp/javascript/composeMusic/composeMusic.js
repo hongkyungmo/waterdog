0b3e448ca4f49d00ef12f8772bd060065c229e9c
YangSeungGIl <bewitchsk@gmail.com>
2016-04-25 16:08:10 +0900

    Progress bar 제거 auto play 연구

--- musicblock/src/main/webapp/javascript/composeMusic/composeMusic.js
+++ musicblock/src/main/webapp/javascript/composeMusic/composeMusic.js
@@ -13,6 +13,7 @@ var swiperMelodyLayer = new Swiper('.layer-melody-block', {
     , spaceBetween: 0
     , grabCursor: false
     , freeMode: true
+    , autoplay : 200
 });
 
 var parent = null;
@@ -72,3 +73,71 @@ $("ol.simple_with_no_drop").sortable({
     , drop: false
 });
 
+var swiperMelodyLayer = new Swiper('.layer-selector', {
+    scrollbar: '.swiper-scrollbar'
+    , scrollbarHide: true
+    , slidesPerView: 'auto'
+    , spaceBetween: 0
+    , grabCursor: false
+    , freeMode: true
+});
+
+$(function () {
+    //    var pointer = $(".progress-bar-pointer").offset().left;
+
+    $("#btn-prev").bind("click", function () {
+        $(".progress-bar-pointer").css("left", "0");
+    });
+
+    $("#btn-play").bind("click", function () {
+        // 포인터의 위치
+//        $("#work-layer").stop().animate({
+//            'textIndent': '320'
+//        }, {
+//            step: function (now, fx) {
+//                now = -500;
+//                $(this).css({
+//                    "transform": "translate3d(" + now + "px,  0px, 0px)"
+//                });
+//            }
+//            , duration: 5000
+//            , easing: 'linear'
+//            , queue: false
+//            , complete: function () {
+//                console.log('Animation is done');
+//            }
+//        }, 'linear');
+//
+//        $("#btn-play").animate({
+//            textIndent: 100
+//        }, {
+//            duration: 10000
+//            , easing: 'linear'
+//            , queue: false
+//        });
+        
+        
+        
+        console.log(swiperMelodyLayer.autoplaying);
+        
+        
+        
+    });
+
+    // 매개면수 e 는 Event형태의 eventObject이다.
+    // this는 선택자의 요소다
+    //    $(".progress-bar-pointer").bind("click", function (e) {
+    //        console.log("포인터 클릭")
+    //        $(".progress-bar-pointer").css("left", e.pageX - 20);
+    //        $(".progress-bar-played").css("width", e.pageX - 10);
+    //    });
+
+    $(".progress-bar-wrapper").bind("click", function (e) {
+        console.log("래퍼 클릭");
+        $(".progress-bar-pointer").css("left", e.pageX - 20);
+        //        $(".progress-bar-played").css("width", e.pageX - 10);
+    });
+
+
+
+});
