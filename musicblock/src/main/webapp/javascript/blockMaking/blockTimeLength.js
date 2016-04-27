2d4434769fcf56ca0cf5ef8cd8bd6b8ab6f7f2ce
hongkyungmo <withious@naver.com>
2016-04-27 20:55:57 +0900

    블럭 플레이타임 코드 분리

--- musicblock/src/main/webapp/javascript/blockMaking/blockTimeLength.js
+++ musicblock/src/main/webapp/javascript/blockMaking/blockTimeLength.js
@@ -1,5 +1,25 @@
 //블럭 길이 조절
-$(function () {
+$(function(){
+	$("#block-time-length-container").bind("click", function(){
+//		var test = "3s";
+//		var testResult = test.replace(/[^0-9]/g, '');
+		if(blockAnimateTime == 10000){
+			blockAnimateTime = 2000;
+		}else{
+			blockAnimateTime += 2000;
+		}
+
+		//버튼에 블럭 재생시간 표시
+		var blockTimeLengthForDisplay=null;
+		blockTimeLengthForDisplay = blockAnimateTime/1000 + "s";
+		$("#block-time-display").text(blockTimeLengthForDisplay);
+	});
+});
+
+
+
+/* 상단바에 둬서 폐기된 코드 */
+/*$(function () {
 	var blockLengthController = '<div id="block-time-length-container"><div id="block-time-length-controlbar"></div></div>';
 
 	$(".COMMON-MENUBAR-ROW").append(blockLengthController);
@@ -29,4 +49,4 @@ $(function () {
 			$("#block-time-length-controlbar").css("left", (timeControlContainerWidth/4)*3);
 		}
 	});
-});
+});*/
