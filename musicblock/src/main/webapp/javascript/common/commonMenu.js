d9e26c677b3332bd060e91dbe38222f3de9ba3ea
YangSeungGIl <bewitchsk@gmail.com>
2016-04-27 13:58:25 +0900

    메뉴바 아이콘 추가

--- musicblock/src/main/webapp/javascript/common/commonMenu.js
+++ musicblock/src/main/webapp/javascript/common/commonMenu.js
@@ -9,13 +9,54 @@ var path = $(location).attr('pathname');
 var element = "<div class='row COMMON-MENUBAR-ROW' id='COMMON-MENUBAR'><div class='col-xs-1 COMMON-MENUBAR-GRID-1'>";
 
 if(path != "/mainpage.html"){
-    element += "<button type='btn' class='btn COMMON-MENUBAR-BTN' id='back'><span class='glyphicon glyphicon-chevron-left' aria-hidden='true' ></span> </button>";
+    element += "<button type='btn' class='btn COMMON-MENUBAR-BTN fa fa-chevron-left fa-2x' id='back'></button>";
 }
 
 element += "</div><div class='col-xs-10 COMMON-MENUBAR-GRID-10'>";
 
-
-var addedComposeMusic = "<div class='row COMPOSE-MENUBAR-ROW'><div class='col-xs-4 COMMON-MENUBAR-GRID-4'></div><div class='col-xs-4 COMMON-MENUBAR-GRID-4'><div class='row COMMON-MENUBAR-ROW'><div class='col-xs-4 COMMON-MENUBAR-GRID-4'><button class='btn COMPOSE-MENUBAR-BTN' data-action='prev' id='btn-prev'><span></span></button></div><div class='col-xs-4 COMMON-MENUBAR-GRID-4'><button class='btn COMPOSE-MENUBAR-BTN' data-action='play' id='btn-play'><span></span></button></div><div class='col-xs-4 COMMON-MENUBAR-GRID-4'><button class='btn COMPOSE-MENUBAR-BTN' data-action='next' id='btn-next'><span></span></button></div></div></div><div class='col-xs-4 COMMON-MENUBAR-GRID-4'></div></div>";
+// composeMusic.html에만 적용되는 MenuBar 버튼 추가
+var addedComposeMusic = 
+    "<div class='row COMPOSE-MENUBAR-ROW'>" + 
+        "<div class='col-xs-4 COMMON-MENUBAR-GRID-4' style='padding-left:0px;'>" + 
+            "<div class='row COMMON-MENUBAR-ROW'>" + 
+                "<div class='col-xs-4 COMMON-MENUBAR-GRID-4'>" + 
+                    "<button class='btn COMPOSE-MENUBAR-BTN fa fa-music fa-2x' id='btn-create' aria-hidden='true'>" + 
+                    "<i class='fa fa-plus-circle' aria-hidden='true'></i>" + 
+                    "</button>" + 
+                "</div>" + 
+                "<div class='col-xs-4 COMMON-MENUBAR-GRID-4'>" + 
+                    "<button class='btn COMPOSE-MENUBAR-BTN fa fa-group fa-2x'' id='btn-community'>" +
+                    "</button>" + 
+                "</div>" + 
+            "</div>" + 
+        "</div>" + 
+            "<div class='col-xs-4 COMMON-MENUBAR-GRID-4'>" + 
+                "<div class='row COMMON-MENUBAR-ROW'>" + 
+                    "<div class='col-xs-4 COMMON-MENUBAR-GRID-4'>" + 
+                        "<button class='btn COMPOSE-MENUBAR-BTN fa fa-step-backward fa-2x' data-action='prev' id='btn-prev'>" + 
+                        "</button>" + 
+                    "</div>" + 
+                    "<div class='col-xs-4 COMMON-MENUBAR-GRID-4'>" + 
+                        "<button class='btn COMPOSE-MENUBAR-BTN fa fa-play fa-2x' data-action='play' id='btn-play'>" + 
+                        "</button>" + 
+                    "</div>" + 
+                "</div>" + 
+            "</div>" + 
+        "<div class='col-xs-4 COMMON-MENUBAR-GRID-4' style='padding-right:0px;'>" +
+            "<div class='row COMMON-MENUBAR-ROW'>" +  
+                "<div class='col-xs-4 COMMON-MENUBAR-GRID-4' style='float:right'>" + 
+                    "<button class='btn COMPOSE-MENUBAR-BTN fa fa-save fa-2x' id='btn-community'>" + 
+                    "<span></span>" + 
+                    "</button>" + 
+                "</div>" + 
+                "<div class='col-xs-4 COMMON-MENUBAR-GRID-4' style='float:right'>" + 
+                    "<button class='btn COMPOSE-MENUBAR-BTN fa fa-folder-open-o fa-2x' id='btn-community'>" + 
+                    "<span></span>" + 
+                    "</button>" + 
+                "</div>" + 
+            "</div>" +  
+        "</div>" + 
+    "</div>";
 
 
 if (path.indexOf("/composeMusic.html") != -1) {
@@ -24,7 +65,7 @@ if (path.indexOf("/composeMusic.html") != -1) {
 
 }
 
-element += "</div><div class='col-xs-1 COMMON-MENUBAR-GRID-1'><button type='button' class='btn COMMON-MENUBAR-BTN' id='menu'><span class='glyphicon glyphicon-menu-hamburger' aria-hidden='true'></span></button></div></div>";
+element += "</div><div class='col-xs-1 COMMON-MENUBAR-GRID-1'><button type='button' class='btn COMMON-MENUBAR-BTN fa fa-navicon fa-2x' id='menu'></button></div></div>";
 
 
 $("body").append(element);
