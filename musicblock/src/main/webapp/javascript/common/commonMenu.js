6b8d3b5209e2112c57824fff8d45132e076c8324
KimYoenyoung <ujoa95@gmail.com>
2016-05-02 21:23:29 +0900

    search아이콘 클릭시 모달 토글~

--- musicblock/src/main/webapp/javascript/common/commonMenu.js
+++ musicblock/src/main/webapp/javascript/common/commonMenu.js
@@ -7,14 +7,14 @@ var path = $(location).attr('pathname');
 
 
 var element = 
-	
-	"<div id='menuPage' >"
+   
+   "<div id='menuPage' >"
 +"<div class='row COMMON-MENUBAR-ROW' id='COMMON-MENUBAR'>"
-+"	<div class='col-xs-1 COMMON-MENUBAR-GRID-1'></div>"
++"   <div class='col-xs-1 COMMON-MENUBAR-GRID-1'></div>"
 +"<div class='col-xs-10 COMMON-MENUBAR-GRID-10'></div>"
 +"<div class='col-xs-1 COMMON-MENUBAR-GRID-1'>"
-+"	<button type='button'"
-+"		class='btn COMMON-MENUBAR-BTN fa fa-remove fa-2x' id='exit'></button>"
++"   <button type='button'"
++"      class='btn COMMON-MENUBAR-BTN fa fa-remove fa-2x' id='exit'></button>"
 +"</div>"
 +"</div>"
 
@@ -22,38 +22,49 @@ var element =
 
 +"<button id='test'>Login</button>"
 
-+"	<div class='ui modal'>"
-+"	<i class='close icon'></i>"
-+"	<div class='header'>Profile Picture</div>"
-+"	<div class='image content'>"
++"   <div class='ui modal'>"
++"   <i class='close icon'></i>"
++"   <div class='header'>Profile Picture</div>"
++"   <div class='image content'>"
 
-+"			<div class='description'>"
-+"			<input type='text' class='input' id='user_login'"
-+"				autocomplete='off' placeholder='Email or Username'> <br>"
-+"			<input type='password' class='input' id='user_pass'"
-+"				autocomplete='off' placeholder='Password'> <br> <input"
-+"				type='checkbox' class='checkbox' id='remember_me'> <label"
-+"				for='remember_me'>Remember me</label>"
-+"		</div>"
-+"	</div>"
-+"	<div class='actions'>"
-+"		<div class='ui black deny button'>login</div>"
-+"	</div>"
++"         <div class='description'>"
++"         <input type='text' class='input' id='user_login'"
++"            autocomplete='off' placeholder='Email or Username'> <br>"
++"         <input type='password' class='input' id='user_pass'"
++"            autocomplete='off' placeholder='Password'> <br> <input"
++"            type='checkbox' class='checkbox' id='remember_me'> <label"
++"            for='remember_me'>Remember me</label>"
++"      </div>"
++"   </div>"
++"   <div class='actions'>"
++"      <div class='ui black deny button'>login</div>"
++"   </div>"
 +"</div>"
 +"</div>"
 +"</div>"
-	
-	
-	
-	
-	
-	+"<div class='row COMMON-MENUBAR-ROW' id='COMMON-MENUBAR'><div class='col-xs-1 COMMON-MENUBAR-GRID-1'>";
+   
+   
+   
+   
+   
+   +"<div class='row COMMON-MENUBAR-ROW' id='COMMON-MENUBAR'><div class='col-xs-1 COMMON-MENUBAR-GRID-1'>";
 
 if(path != "/mainpage.html"){
     element += "<button type='btn' class='btn COMMON-MENUBAR-BTN fa fa-chevron-left fa-2x' id='back'></button>";
 }
 
 element += "</div><div class='col-xs-10 COMMON-MENUBAR-GRID-10'>";
+
+var addCommunity=
+		"<div class='row COMMON-MENUBAR-ROW' id='COMMON-MENUBAR'>" +
+    		"<div class='col-xs-1 COMMON-MENUBAR-GRID-1' style='float:right'>" + 
+    		 "<button class='btn COMPOSE-MENUBAR-BTN fa fa-search fa-2x' id='btn-search'>" + 
+                "<button class='btn COMPOSE-MENUBAR-BTN fa fa-search fa-2x' id='btn-search'>" + 
+                "<span></span>" + 
+                "</button>" + 
+            "</div>" + 
+           "</div>";
+
 
 // composeMusic.html에만 적용되는 MenuBar 버튼 추가
 var addedComposeMusic = 
@@ -102,8 +113,8 @@ var addedComposeMusic =
 
 if (path.indexOf("/composeMusic.html") != -1) {
     element += addedComposeMusic;
-} else {
-
+} else if(path.indexOf("/community.html") != -1) {
+	element += addCommunity;
 }
 
 element += "</div><div class='col-xs-1 COMMON-MENUBAR-GRID-1'><button type='button' class='btn COMMON-MENUBAR-BTN fa fa-navicon fa-2x' id='menu'></button></div></div>";
@@ -113,49 +124,48 @@ $("body").append(element);
 
 $(function() {
 
-	$("#back").bind("click", function() {
-		console.log("뒤로가기 누름ㅋㅋ");
-	});
+   $("#back").bind("click", function() {
+      console.log("뒤로가기 누름ㅋㅋ");
+   });
 
-	$("#menu").bind("click", function() {
-		console.log("메뉴버튼 누름ㅋㅋ");
-		wrapWindowBymenuPage();
+   $("#menu").bind("click", function() {
+      console.log("메뉴버튼 누름ㅋㅋ");
+      wrapWindowBymenuPage();
 
-	});
-	
-	$("#exit").bind("click",function(){
-		console.log("exit 누름ㅋㅋ");
-		$("#menuPage").hide();
-	});
-	
-	$('#test').bind("click",function() {
-		console.log("login 누름ㅋㅋ")
-		$('.ui.modal').modal('show');
-	});
+   });
+   
+   $("#exit").bind("click",function(){
+      console.log("exit 누름ㅋㅋ");
+      $("#menuPage").hide();
+   });
+   
+   $('#test').bind("click",function() {
+      console.log("login 누름ㅋㅋ")
+      $('.ui.modal').modal('show');
+   });
 });
 
 function wrapWindowBymenuPage() {
-	//화면의 높이와 너비를 구한다.
-	var maskHeight = $(document).height();
-	var maskWidth = $(window).width();
+   //화면의 높이와 너비를 구한다.
+   var maskHeight = $(document).height();
+   var maskWidth = $(window).width();
 
-	//마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채운다.
-	$('#menuPage').css({
-		'width' : maskWidth,
-		'height' : maskHeight,
-		'z-index' : 150
-	});
-	//마스크의 투명도 처리
+   //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채운다.
+   $('#menuPage').css({
+      'width' : maskWidth,
+      'height' : maskHeight,
+      'z-index' : 150
+   });
+   //마스크의 투명도 처리
 
-	$('#menuPage').fadeTo("slow", 1);
+   $('#menuPage').fadeTo("slow", 1);
 }
 
 $(document).ready(function() {
-	//wrapWindowBymenuPage();
-	//불투명 배경 띄우기
-	$('.openmenuPage').click(function(e) {
-		e.preventDefault();
-		wrapWindowBymenuPage();
-	});
+   //wrapWindowBymenuPage();
+   //불투명 배경 띄우기
+   $('.openmenuPage').click(function(e) {
+      e.preventDefault();
+      wrapWindowBymenuPage();
+   });
 });
-
