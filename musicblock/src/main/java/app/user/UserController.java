1e4dcf57f4add4ad64c9145bbfc69f10f49d6ab2
keheunhye <asopn1994@daum.net>
2016-05-03 19:59:57 +0900

    ajax 통신 1

--- musicblock/src/main/java/app/user/UserController.java
+++ musicblock/src/main/java/app/user/UserController.java
@@ -1,5 +1,33 @@
 package app.user;
 
-public class UserController {
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import service.domain.User;
+import service.user.UserService;
+
+@Controller
+@RequestMapping("/user/*")
+public class UserController {
+	@Autowired
+	@Qualifier("userServiceImpl")
+	private UserService userService;
+
+	public UserController() {
+		System.out.println(this.getClass());
+	}
+	
+	@RequestMapping(value = "userLogin", method = RequestMethod.POST)
+	public User getJsonUserPOST(@RequestBody Map map) throws Exception {
+		System.out.println("(/user/userLogin)RequestBody로 전달받은 String(JSON) : "+map);
+
+		User user = new User();
+		return user;
+	}
 }
