9892a4a8d31b4648bfcb97c3afca2296f9d048a9
hongkyungmo <withious@naver.com>
2016-05-07 03:46:25 +0900

    indexedDB에 dummy 데이터로 음악 저장

--- musicblock/src/main/webapp/javascript/emotion/indexedDBForMusicEmotion.js
+++ musicblock/src/main/webapp/javascript/emotion/indexedDBForMusicEmotion.js
@@ -24,6 +24,32 @@ function addMusic(){
 	}
 }
 
+
+function dummyAddMusic(){
+	var transaction = db.transaction(["musicTable"], "readwrite");
+	var objectStore = transaction.objectStore("musicTable");
+	
+	var musicArr = new Array();
+	musicArr[0]="1&42,33,34/1&41,27,46";
+	musicArr[1]="1&2,3,4/1&4,2,4";
+	musicArr[2]="1&2,23,14/1&4,7,6";
+	musicArr[3]="1&11,31,14/1&4,27,24";
+	musicArr[4]="1&22,42,24/1&41,27,46";
+	musicArr[5]="1&33,33,3/1&1,27,16";
+	musicArr[6]="1&44,24,44/1&11,28,26";
+	musicArr[7]="1&13,21,1/1&43,33,36";
+	musicArr[8]="1&22,12,24/1&11,2,46";
+	musicArr[9]="1&31,13,3/1&21,7,6";
+	for(var i=0;i<10;i++){
+		request = objectStore.add({musicInfo:musicArr[i]});
+		request.onsuccess = function(event){
+			console.log("IndexedDB에 음악을 성공적으로 저장했습니다.");
+			getAllMusics();
+		};
+	}
+}
+
+
 function getAllMusics(){
 	var transaction = db.transaction(["musicTable"], "readonly");
 	var objectStore = transaction.objectStore("musicTable");
