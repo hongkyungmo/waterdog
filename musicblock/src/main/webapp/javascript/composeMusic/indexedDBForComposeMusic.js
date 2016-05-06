d39aba88f6fac24661dd4732d76c317b9305eabb
hongkyungmo <withious@naver.com>
2016-05-07 00:39:42 +0900

    DB 버전수정을 위해 초기화 부분 공통모듈화(DB 버전을 3으로 바꿨음. 혹시라도 이상동작 있는 페이지 있으면 알려주세요)

--- musicblock/src/main/webapp/javascript/composeMusic/indexedDBForComposeMusic.js
+++ musicblock/src/main/webapp/javascript/composeMusic/indexedDBForComposeMusic.js
@@ -1,15 +1,5 @@
 /*indexedDB for Compose Music*/
 
-//variables for IndexedDB
-// In the following line, you should include the prefixes of implementations you want to test.
-window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-// DON'T use "var indexedDB = ..." if you're not in a function.
-// Moreover, you may need references to some window.IDB* objects:
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
-    // (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
-var request = window.indexedDB.open("musicBlockDB", 2);
-var db;
 var blockNum = 0;
 
 var repo = $("#my-blocks");
@@ -17,17 +7,6 @@ var repo = $("#my-blocks");
 
 //functions related to IndexedDB
 $(function () {
-
-    request.onupgradeneeded = function (event) {
-        console.log("onupgradeneeded : DB initialized / created");
-        db = event.target.result;
-        //오브젝트 스토어가 테이블 개념, keyPath가 시퀀스 개념
-        store = db.createObjectStore("blockTestTable", {
-            keyPath: "id"
-            , autoIncrement: true
-        });
-    }
-
     request.onsuccess = function (event) {
 //        console.log("indexedDB onsuccess : DB loaded successfully");
         db = event.target.result;
@@ -35,16 +14,6 @@ $(function () {
         getAllBlocks();
 
     }
-
-    request.onerror = function (event) {
-        alert("indexedDB onerror");
-    }
-
-    request.onready = function (event) {
-        alert("indexedDB onready");
-    }
-
-
 });
 
 
--- musicblock/src/main/webapp/javascript/emotion/indexedDBForMusicEmotion.js
+++ musicblock/src/main/webapp/javascript/emotion/indexedDBForMusicEmotion.js
@@ -1,40 +1,9 @@
 /* indexedDB for emotion */
-//variables for IndexedDB
-// In the following line, you should include the prefixes of implementations you want to test.
-window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-// DON'T use "var indexedDB = ..." if you're not in a function.
-// Moreover, you may need references to some window.IDB* objects:
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
-// (Mozilla has never prefixed these objects, so we don't need window.mozIDB*)
-
-if (!window.indexedDB) {
-	window.alert("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.")
-}
-
-var request = window.indexedDB.open("musicBlockDB", 2);
-var db;
-
 //functions related to IndexedDB
 $(function(){
-	request.onupgradeneeded = function(event){
-		console.log("onupgradeneeded : DB initialized / created");
-		db = event.target.result;
-		//오브젝트 스토어가 테이블 개념, keyPath가 시퀀스 개념
-		store = db.createObjectStore("musicTable", {keyPath:"id", autoIncrement: true});
-	}
-
 	request.onsuccess = function(event){
 		console.log("onsuccess : DB loaded successfully");
 		db = event.target.result;
-	}
-
-	request.onerror = function(event){
-		alert("onerror : " + event.target.error);
-	}
-
-	request.onready = function(event){
-		console.log("onready");
 	}
 });
 
@@ -67,4 +36,3 @@ function getAllMusics(){
 		}
 	}
 }
-/* end of functions related to IndexedDB */
