472528b09f0271ed8d833602b3de1adfe13cb94c
YangSeungGIl <bewitchsk@gmail.com>
2016-05-09 17:25:16 +0900

    충돌나고싶스무니다

--- musicblock/src/main/webapp/javascript/common/commonMenu.js
+++ musicblock/src/main/webapp/javascript/common/commonMenu.js
@@ -1,66 +1,61 @@
 /**
  * 공통모듈 : 페이지 로딩시 자동으로 불러내는 메뉴바 추가
  */
-
-
-//해당 URI를 가져와서 공통 Module에 넣을 Element를 동적으로 추가
+// 해당 URI를 가져와서 공통 Module에 넣을 Element를 동적으로 추가
 var path = $(location).attr('pathname');
 
 
+
 var element = 
+	
 	"<div id='menuPage' >"
-	+"<div class='row COMMON-MENUBAR-ROW' id='COMMON-MENUBAR'>"
-	+"	<div class='col-xs-1 COMMON-MENUBAR-GRID-1'></div>"
-	+"<div class='col-xs-10 COMMON-MENUBAR-GRID-10'></div>"
-	+"<div class='col-xs-1 COMMON-MENUBAR-GRID-1'>"
-	+"	<button type='button'"
-	+"		class='btn COMMON-MENUBAR-BTN fa fa-remove fa-2x' id='exit'></button>"
-	+"</div>"
-	+"</div>"
++"<div class='row COMMON-MENUBAR-ROW' id='COMMON-MENUBAR'>"
++"	<div class='col-xs-1 COMMON-MENUBAR-GRID-1'></div>"
++"<div class='col-xs-10 COMMON-MENUBAR-GRID-10'></div>"
++"<div class='col-xs-1 COMMON-MENUBAR-GRID-1'>"
++"	<button type='button'"
++"		class='btn COMMON-MENUBAR-BTN fa fa-remove fa-2x' id='exit'></button>"
++"</div>"
++"</div>"
 
-	+"<div id='loginPage'>"
++"<div id='loginPage'>"
 
-	+"<center>"
-	+"<button id='test' class='customButton'>Login</button>"
-	+"<br>"
-	+"<button id='test3' class='customButton'>BlockList</button>"
-	+"<button id='test4' class='customButton'>MusicList</button>"
-	+"<button id='test5' class='customButton'>Community</button>"
-	+"<br>"
-	+"<button id='test6' class='customButton'>사용설명서</button>"
-	+"<button id='test7' class='customButton'>설정</button>"
-	+"<button id='test8' class='customButton'>기타</button>"
-	+"</center>"
++"<center>"
++"<button id='test' class='customButton'>Login</button>"
++"<br>"
++"<button id='test3' class='customButton'>BlockList</button>"
++"<button id='test4' class='customButton'>MusicList</button>"
++"<button id='test5' class='customButton'>Community</button>"
++"<br>"
++"<button id='test6' class='customButton'>사용설명서</button>"
++"<button id='test7' class='customButton'>설정</button>"
++"<button id='test8' class='customButton'>기타</button>"
++"</center>"
 
-	+"	<div class='ui modal'>"
-	+"	<i class='close icon'></i>"
-	+"	<div class='header'>Profile Picture</div>"
-	+"	<div class='image content'>"
++"	<div class='ui modal'>"
++"	<i class='close icon'></i>"
++"	<div class='header'>Profile Picture</div>"
++"	<div class='image content'>"
 
-	+"			<div class='description'>"
-	+"			<input type='text' class='input' id='user_login'"
-	+"				autocomplete='off' placeholder='Email or Nickname'> <br>"
-	+"			<input type='password' class='input' id='user_pass'"
-	+"				autocomplete='off' placeholder='Password'> <br> <input"
-	+"				type='checkbox' class='checkbox' id='remember_me'> <label"
-	+"				for='remember_me'>Remember me</label>"
-	+"		</div>"
-	+"	</div>"
-	+"	<div class='actions'>"
-	+"		<div class='ui black deny button'>login</div>"
-	+"	</div>"
-	+"</div>"
-	+"</div>"
-	+"</div>"
-
-
-
-
-
-	+"<div class='row COMMON-MENUBAR-ROW' id='COMMON-MENUBAR'><div class='col-xs-1 COMMON-MENUBAR-GRID-1'>";
++"			<div class='description'>"
++"			<input type='text' class='input' id='user_login'"
++"				autocomplete='off' placeholder='Email or Nickname'> <br>"
++"			<input type='password' class='input' id='user_pass'"
++"				autocomplete='off' placeholder='Password'> <br> <input"
++"				type='checkbox' class='checkbox' id='remember_me'> <label"
++"				for='remember_me'>Remember me</label>"
++"		</div>"
++"	</div>"
++"	<div class='actions'>"
++"		<div class='ui black deny button'>login</div>"
++"	</div>"
++"</div>"
++"</div>"
++"</div>"
++"<div class='row COMMON-MENUBAR-ROW' id='COMMON-MENUBAR'><div class='col-xs-1 COMMON-MENUBAR-GRID-1'>";
 
 if(path != "/mainpage.html"){
-	element += "<button type='btn' class='btn COMMON-MENUBAR-BTN fa fa-chevron-left fa-2x' id='back'></button>";
+    element += "<button type='btn' class='btn COMMON-MENUBAR-BTN fa fa-chevron-left fa-2x' id='back'></button>";
 }
 
 element += "</div><div class='col-xs-10 COMMON-MENUBAR-GRID-10'>";
@@ -74,54 +69,10 @@ element += "</div><div class='col-xs-10 COMMON-MENUBAR-GRID-10'>";
                 "</button>" + 
             "</div>" + 
            "</div>";
- */
-//composeMusic.html에만 적용되는 MenuBar 버튼 추가
-var addedComposeMusic = 
-	"<div class='row COMPOSE-MENUBAR-ROW'>" + 
-	"<div class='col-xs-4 COMMON-MENUBAR-GRID-4' style='padding-left:0px;'>" + 
-	"<div class='row COMMON-MENUBAR-ROW'>" + 
-	"<div class='col-xs-4 COMMON-MENUBAR-GRID-4'>" + 
-	"<button class='btn COMPOSE-MENUBAR-BTN fa fa-music fa-2x' id='btn-create' aria-hidden='true'>" + 
-	"<i class='fa fa-plus-circle' aria-hidden='true'></i>" + 
-	"</button>" + 
-	"</div>" + 
-	"<div class='col-xs-4 COMMON-MENUBAR-GRID-4'>" + 
-	"<button class='btn COMPOSE-MENUBAR-BTN fa fa-group fa-2x' id='btn-community'>" +
-	"</button>" + 
-	"</div>" + 
-	"</div>" + 
-	"</div>" + 
-	"<div class='col-xs-4 COMMON-MENUBAR-GRID-4'>" + 
-	"<div class='row COMMON-MENUBAR-ROW'>" + 
-	"<div class='col-xs-4 COMMON-MENUBAR-GRID-4'>" + 
-	"<button class='btn COMPOSE-MENUBAR-BTN fa fa-step-backward fa-2x' data-action='prev' id='btn-prev'>" + 
-	"</button>" + 
-	"</div>" + 
-	"<div class='col-xs-4 COMMON-MENUBAR-GRID-4'>" + 
-	"<button class='btn COMPOSE-MENUBAR-BTN fa fa-play fa-2x' data-action='play' id='btn-play'>" + 
-	"</button>" + 
-	"</div>" + 
-	"</div>" + 
-	"</div>" + 
-	"<div class='col-xs-4 COMMON-MENUBAR-GRID-4' style='padding-right:0px;'>" +
-	"<div class='row COMMON-MENUBAR-ROW'>" +  
-	"<div class='col-xs-4 COMMON-MENUBAR-GRID-4' style='float:right'>" + 
-	"<button class='btn COMPOSE-MENUBAR-BTN fa fa-save fa-2x' id='btn-save'>" + 
-	"<span></span>" + 
-	"</button>" + 
-	"</div>" + 
-	"<div class='col-xs-4 COMMON-MENUBAR-GRID-4' style='float:right'>" + 
-	"<button class='btn COMPOSE-MENUBAR-BTN fa fa-folder-open-o fa-2x' id='btn-load'>" + 
-	"<span></span>" + 
-	"</button>" + 
-	"</div>" + 
-	"</div>" +  
-	"</div>" + 
-	"</div>";
-
+           */
 
 if (path.indexOf("/composeMusic.html") != -1) {
-	element += addedComposeMusic;
+//    element += addedComposeMusic;
 }else if(path.indexOf("/community.html") != -1) {
 	//element += addCommunity;
 }
@@ -133,7 +84,6 @@ $("body").append(element);
 
 $(function() {
 
-
 	$("#back").bind("click", function() {
 		history.back();
 		console.log("뒤로가기 누름ㅋㅋ");
@@ -144,12 +94,12 @@ $(function() {
 		wrapWindowBymenuPage();
 
 	});
-
+	
 	$("#exit").bind("click",function(){
 		console.log("exit 누름ㅋㅋ");
 		$("#menuPage").hide();
 	});
-
+	
 	$('#test').bind("click",function() {
 		console.log("login 누름ㅋㅋ")
 		$('.ui.modal').modal('show');
@@ -202,36 +152,36 @@ function serverLogin() {
 	var transRmemember = (document.getElementById("remember_me")).checked;
 
 	$
-	.ajax({ //$.post(), $.get(), $.getJSON 등도 있음
-		url : 'user/userLogin',
-		type : 'POST', //Request하는 방식.
-		data : JSON.stringify({ //JSON.stringify를 해줘야 제대로 된 형태의 JSON이 날아감
-			user : transUser,
-			pass : transPass,
-			remember : transRmemember
-		}),
-		dataType : "json", //Response로 오는 방식. Request 타입을 지정하는 것으로 착각하기 쉬우므로 주의.
-		contentType : 'application/json;charset=UTF-8', //POST방식일 때 사용. 인코딩 안해주면 한글 깨져서 전송됨
-		success : function(data, status) {
-			console.log(status);
-			console.log("JSONData : " + JSON.stringify(data));
-			if (data['message'] != null) {
+			.ajax({ //$.post(), $.get(), $.getJSON 등도 있음
+				url : 'user/userLogin',
+				type : 'POST', //Request하는 방식.
+				data : JSON.stringify({ //JSON.stringify를 해줘야 제대로 된 형태의 JSON이 날아감
+					user : transUser,
+					pass : transPass,
+					remember : transRmemember
+				}),
+				dataType : "json", //Response로 오는 방식. Request 타입을 지정하는 것으로 착각하기 쉬우므로 주의.
+				contentType : 'application/json;charset=UTF-8', //POST방식일 때 사용. 인코딩 안해주면 한글 깨져서 전송됨
+				success : function(data, status) {
+					console.log(status);
+					console.log("JSONData : " + JSON.stringify(data));
+					if (data['message'] != null) {
 
-				console.log()
-				$("#loginPage")
-				.append(
-						"<div id='save-popup'><center>"+data['message']+"</center></div>");
-			} else {
+						console.log()
+						$("#loginPage")
+								.append(
+										"<div id='save-popup'><center>"+data['message']+"</center></div>");
+					} else {
 
-				console.log(data['message']);
-				$("#loginPage")
-				.append(
-						"<div id='save-popup'><center>"+data['message']+"</center></div>");
-
-			}
-		},
-		error : function(status) {
-			console.log(status);
-		}
-	});
+						console.log(data['message']);
+						$("#loginPage")
+						.append(
+								"<div id='save-popup'><center>"+data['message']+"</center></div>");
+			
+					}
+				},
+				error : function(status) {
+					console.log(status);
+				}
+			});
 };
