5e5fd39447243304596775de3e90b4dcba0c0db5
hongkyungmo <withious@naver.com>
2016-05-11 10:19:09 +0900

    메인페이지 음 끊김 현상 해결 완료

--- musicblock/src/main/webapp/javascript/mainpage/playSound.js
+++ musicblock/src/main/webapp/javascript/mainpage/playSound.js
@@ -12,6 +12,7 @@ var blockWalkerLimit;
 var noteWalkerLimit;
 //블럭+블럭+블럭... 배열
 var blockArr;
+var musicNotesArr;
 
 
 //오디오컨텍스트 설정 및 생성
@@ -19,11 +20,17 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioContext = new AudioContext();
 
 //오실레이터, 게인 생성
-var oscillator = audioContext.createOscillator();
+//var oscillator = audioContext.createOscillator();
 var gain = audioContext.createGain();
 
+var oscArr = new Array();
+for(var i=0;i<48;i++){
+	oscArr[i] = audioContext.createOscillator();
+	oscArr[i].start(0);
+}
+
 //오실레이터 설정
-oscillator.type = 'sine'; //파형의 형태 sine, square, sawtooth, triangle, custom 등
+/*oscillator.type = 'sine'; //파형의 형태 sine, square, sawtooth, triangle, custom 등
 oscillator.frequency.value = 300; //주파수
 oscillator.connect(gain); //게인과 연결
 
@@ -32,7 +39,7 @@ gain.gain.value = mainVolume; //볼륨
 gain.connect(audioContext.destination); //데스티네이션(스피커)와 연결
 
 //오실레이터 시작
-oscillator.start();
+oscillator.start();*/
 
 //notecode(1~48)을 주파수로 변환해주는 함수
 var noteCodeToFreq = function (noteVal) {
@@ -40,11 +47,18 @@ var noteCodeToFreq = function (noteVal) {
     return 130.8128 * Math.pow(1.0594630943592952645618252949463, noteVal - 1);
 }
 
-//주파수를 넣으면 음을 재생해주는 함수
+//노트코드를 넣으면 음을 재생해주는 함수
 var playNote = function (noteVal) {
-    oscillator.frequency.value = noteVal;
-    
-    gain.gain.value = mainVolume;
+	oscArr[noteVal].frequency.value = noteCodeToFreq(noteVal);
+	
+	gain.gain.value = mainVolume;
+    oscArr[noteVal].connect(gain);
+    gain.connect(audioContext.destination);
+}
+
+//노트코드를 넣으면 음을 재생해 주는 함수
+var stopNote = function (noteVal) {
+	oscArr[noteVal].disconnect(gain);
 }
 
 var playBlock = function(){
@@ -57,15 +71,17 @@ var playBlock = function(){
 	//초와 음 집합으로 쪼개기
 	var secAndNotesArr = blockArr[blockWalker++].split('&');
 	var musicSec = secAndNotesArr[0];
-	var musicNotesArr = secAndNotesArr[1].split(',');
+	musicNotesArr = secAndNotesArr[1].split(',');
 	
 	noteWalker = 0;
 	noteWalkerLimit = musicNotesArr.length;
-	playNote(noteCodeToFreq(musicNotesArr[noteWalker++]));//첫 음 재생
+	playNote(musicNotesArr[noteWalker++]);//첫 음 재생
 	noteTimerClearer = setInterval(function(){
 		if(noteWalker < noteWalkerLimit){
-			playNote(noteCodeToFreq(musicNotesArr[noteWalker++]));
+			stopNote(musicNotesArr[noteWalker-1]);
+			playNote(musicNotesArr[noteWalker++]);
 		}else{
+			stopNote(musicNotesArr[noteWalker-1]);
 			mainVolume = 0;
 			gain.gain.value = mainVolume;
 			clearInterval(noteTimerClearer);
@@ -110,9 +126,11 @@ $(function(){
 		}else{//현재 재생 중인 음악 있음
 			if(currentClickedIndex == currentPlayingIndexForSound){// Stop 시나리오
 				console.log("Stop시나리오");
+				stopNote(musicNotesArr[noteWalker-1]);
 				currentPlayingIndexForSound = -1.1;
 			}else{//Play 시나리오
 				console.log("Play시나리오 : 기존 재생되던 것 중지하고, 현재 음악 재생 시작");
+				stopNote(musicNotesArr[noteWalker-1]);
 				currentPlayingIndexForSound = currentClickedIndex;
 				stopMusic();
 				playMusic(currentClickedIndex);
@@ -120,8 +138,6 @@ $(function(){
 		}
 	});
 });
-
-
 
 //정지 시나리오
 $(function(){
