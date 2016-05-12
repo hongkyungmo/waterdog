2858ed19fd819ab64e8bb1abe0e1916f4b24ef00
hongkyungmo <withious@naver.com>
2016-05-13 02:03:42 +0900

    링크 및 주석 추가 / mp3 모바일에서 재생되지 않는 문제 해결 중

--- musicblock/src/main/webapp/javascript/blockMaking/soundManager.js
+++ musicblock/src/main/webapp/javascript/blockMaking/soundManager.js
@@ -10,6 +10,10 @@ var audio = new Audio();
 var context = new AudioContext();
 var analyser = context.createAnalyser();
 var source = context.createMediaElementSource(audio);
+//모바일 browser에서 web audio api를 이용하면 일반적 방법으로는 재생이 되지 않고 있음.
+//로컬이 아닌 경우 createMediaStreamSource를 사용해야 할 듯.
+//Ajax로 서버로부터 mp3를 가져온 후 decoding을 하고, 헤더도 설정하고 등등의 추가 작업이 필요하다는 카더라가 있음...
+//일단 폰갭이나 웹뷰를 씌워서 로컬의 mp3를 재생하는 부분을 구현해 보고, 안 되면 다른 방법을 찾아봐야 함.
 var gain = context.createGain();
 
 //건반 클릭
