47a8f44fa7fad56edc453c523791c16cb9aa9472
KimYoenyoung <ujoa95@gmail.com>
2016-05-21 14:41:33 +0900

    공통메뉴 index로 수정

--- musicblock/src/main/webapp/javascript/common/commonMenu.js
+++ musicblock/src/main/webapp/javascript/common/commonMenu.js
@@ -14,7 +14,7 @@ var logout= "<button type='button' id='logout' class='menuButton btn btn-primary
 var element = 
 	"<div class='row COMMON-MENUBAR-ROW' id='COMMON-MENUBAR'><div class='col-xs-1 COMMON-MENUBAR-GRID-1'>";
 
-if(path != "/mainpage.html"){
+if(path != "/index.html"){
 	element += "<button type='btn' class='btn COMMON-MENUBAR-BTN fa fa-chevron-left fa-2x' id='back'></button>";
 }
 
